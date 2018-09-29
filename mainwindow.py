908c8b7f300f21ea462ce50d5f82a4497f06a4a0
Darkclainer <Darkclainer@gmail.com>
2018-09-29 19:05:30 +0300

    Remove test metaword

--- mainwindow.py
+++ mainwindow.py
@@ -6,12 +6,6 @@ from .metawordfinder import MetawordFinder
 from .ankiword import metawordToAnkiwordList
 from .ankiwordwidget import AnkiwordWidget
 
-def loadTestMetaword():
-    with open('/home/dio/.local/share/Anki2/addons21/englishWordManager/testmetaword.json') as f:
-        metawordJson = json.load(f)
-
-    return Metaword.fromSON(metawordJson)
-
 class MainWindow(QWidget):
     def __init__(self, *, ankiInterface, **kargs):
         super().__init__(**kargs)
@@ -34,7 +28,6 @@ class MainWindow(QWidget):
 
         self.ankiwordWidget = AnkiwordWidget(ankiInterface=self.ankiInterface)
         vbox.addWidget(self.ankiwordWidget)
-        self.setMetaword(loadTestMetaword())
 
         self.show()
 
